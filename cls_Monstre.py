3595e040dc3bd079fb7aea9eb5c2b04541485737
LavLab <angel.lavigne@outlook.com>
2020-07-15 20:06:20 +0200

    bar de vie en calque superieur

--- cls_Monstre.py
+++ cls_Monstre.py
@@ -6,7 +6,7 @@ class cls_Monstre(pygame.sprite.Sprite):
     def __init__(self, Jeu):
         super().__init__()
         self.Jeu = Jeu
-        self.vie = 100
+        self.vie = 50
         self.max_vie = 100
         self.puissance = 5
         self.vitesse = 2
@@ -16,9 +16,16 @@ class cls_Monstre(pygame.sprite.Sprite):
         self.rect.y = 550
         
     def bar_vie(self, Ecran):
+        # Bar de vie total
+        bar_couleur = (111, 210, 46)
+        bar_position = [self.rect.x + 10, self.rect.y - 20, self.max_vie, 5]
+        pygame.draw.rect(Ecran, bar_couleur, bar_position)
+        
+        # Bar de vie actuel
         bar_couleur = (111, 210, 46)
         bar_position = [self.rect.x + 10, self.rect.y - 20, self.vie, 5]
         pygame.draw.rect(Ecran, bar_couleur, bar_position)
+        
         
     def deplacement(self):
         if not self.Jeu.verifier_colision(self, self.Jeu.les_joueurs):
